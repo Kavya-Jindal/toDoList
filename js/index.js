dfa1cb9a52cf6f177777671fd5bf6902377a622e
LAPTOP-7682VIAD\hp <kavya18cv@gmail.com>
2022-09-27 14:27:48 +0530

    edited todo function

--- js/index.js
+++ js/index.js
@@ -1,26 +1,32 @@
-var container=document.querySelector('.container')
-var value1=document.querySelector('.input')
-var add1=document.querySelector('.add')
+var container = document.querySelector('.container')
+var value1 = document.querySelector('.input')
+var add1 = document.querySelector('.add')
+
+add1.addEventListener('click',plus)
+window.addEventListener('keypress',(e)=>{
+    if(e.key==="Enter"){
+        plus()
+    }
+})
+
 function todo(){
     var li=document.createElement('div')
+
     var input=document.createElement('input')
     input.type="text"
     input.value=value1.value
     input.classList.add('itemInput')
+
     var remove=document.createElement('button')
     remove.classList.add('remove')
     remove.innerHTML='<i class="fa-solid fa-xmark"></i>'
+
+    
     li.append(remove)
     remove.addEventListener('click',()=>li.remove())
     container.appendChild(li)
     li.appendChild(input)
 }
-add1.addEventListener('click',plus)
-window.addEventListener('keydown',(e)=>{
-    if(e.which==13){
-        plus()
-    }
-})
 function plus(){
     if(value1.value!=""){
         todo()
